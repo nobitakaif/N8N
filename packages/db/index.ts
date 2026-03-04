2164dbfddea17064f5995e2e8c2382ba7ea94463
mohd kaif <mk2818356@gmail.com>
2026-03-05 03:13:55 +0530

    added db

---
+++ packages/db/index.ts
@@ -0,0 +1,64 @@
+
+import mongoose, {Schema} from "mongoose";
+
+const UserSchema = new Schema({
+    email : {type : String, required : true, unique : true},
+    password : {type : String, required : true}
+})
+
+const EdgeSchema = new Schema({
+    id : { type : String, required : true}, 
+    source : {type : String, required :true},
+    target : {type : String, required :true}
+}, {
+    _id : false // this will not generate automatic _id 
+})
+
+const PositionSchema = new Schema({
+    x : { type : String, required :  true},
+    y : { type : String, required : true}
+},{
+    _id : false
+})
+
+const NodeDataSchema = new Schema({
+    kind : { type : String, enum : ['ACTION' , 'TRIGGER']},
+    metadata : Schema.Types.Mixed
+}, { _id : false })
+
+const NodeWorkflowSchema = new Schema({
+    id : {type : String, required : true},
+    type : { type : mongoose.Types.ObjectId, ref : 'Nodes'},
+    data : NodeDataSchema, // Schema.Types.Mixed => any type 
+    position : PositionSchema,
+    credentials : Schema.Types.Mixed
+}, {
+    _id : false
+})
+
+const WorkflowSchema = new Schema({
+    userId : { type : mongoose.Types.ObjectId, required : true, ref : 'Users'},
+    nodes : [NodeWorkflowSchema],
+    edges : [EdgeSchema]
+})
+
+const CredentialsTypeSchema = new Schema({
+    title : { type : String, required : true},
+    required : { type : Boolean, required : true}
+})
+
+const NodeSchema  = new Schema({
+    title : { type : String, required : true},
+    description : { type : String, required : true},
+    type : { type : String, enum : ['ACTION', 'TRIGGER'], requied : true},
+    credentialsType : [CredentialsTypeSchema],
+})
+
+const ExecutionSchema = new Schema({
+    workflowId : { type : mongoose.Types.ObjectId, required : true, ref : 'Workflows'},
+    status : { type : String, enum : ['PENDING', 'SUCCESS', 'FAILURE']}
+})
+
+export const UserModel = mongoose.model("Users", UserSchema)
+export const WorkflowModel = mongoose.model('Workflows', WorkflowSchema)
+export const NodeModel = mongoose.model('Node', NodeSchema)
